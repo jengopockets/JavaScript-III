6f2c2fff44f175a58c0617872462ebd03ddbfb98
Jengopockets <jengopockets@gmail.com>
2019-05-08 15:03:33 -0400

    started this.js

--- assignments/this.js
+++ assignments/this.js
@@ -1,8 +1,8 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
+* 1. Global Binding will cause "this" to point to window /console object 
+* 2. Implicit Binding points to the object Left of
 * 3. 
 * 4. 
 *
@@ -13,9 +13,28 @@
 
 // code example for Window Binding
 
+function helloWorld(name) {
+    console.log(this);
+    return name;
+}
+helloWorld("d'sNutz");
+
 // Principle 2
 
 // code example for Implicit Binding
+const declareAlly = obj => {
+    obj.pledge = function() {
+        console.log(`I ${this.name} Pledge Loyalty to House Stark`);
+        console.log(this)
+    };
+};
+const houseDs = { name: 'Jared'};
+const houseNuts = { name: 'Joe'};
+declareAlly(houseDs);
+declareAlly(houseNuts);
+
+houseDs.pledge();
+houseNuts.pledge();
 
 // Principle 3
 
