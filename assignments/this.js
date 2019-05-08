c53848d7812db9a64d25260e778ba7879cfe5b7a
Jengopockets <jengopockets@gmail.com>
2019-05-08 19:14:52 -0400

    almost done

--- assignments/this.js
+++ assignments/this.js
@@ -4,7 +4,7 @@
 * 1. Global Binding will cause "this" to point to window /console object 
 * 2. Implicit Binding points to the next object Left of "this".
 * 3. New Binding Uses a Constructor To create new objects
-* 4. 
+* 4. Explicit
 *
 * write out a code example of each explanation above
 */
@@ -57,3 +57,14 @@ traitor.speak();
 // Principle 4
 
 // code example for Explicit Binding
+const person ={
+    name:"Your Mom"
+}
+
+function intro(chore1,chore2,chore3){
+    return `Hello I am ${person.name} and you need to ${chore1}, ${chore2}, ${chore3}`;
+}
+
+const chores = ["clean your room","sweep", "mop"]
+
+console.log(intro.apply(person,chores));
