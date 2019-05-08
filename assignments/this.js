10b0a29b342c49f8088cafbbc30ccecb9d8be954
Jengopockets <jengopockets@gmail.com>
2019-05-08 16:04:21 -0400

    worked on this.js

--- assignments/this.js
+++ assignments/this.js
@@ -2,8 +2,8 @@
 * in your own words. explain the four principle for the "this" keyword below.
 *
 * 1. Global Binding will cause "this" to point to window /console object 
-* 2. Implicit Binding points to the object Left of
-* 3. 
+* 2. Implicit Binding points to the next object Left of "this".
+* 3. New Binding Uses a Constructor To create new objects
 * 4. 
 *
 * write out a code example of each explanation above
@@ -39,6 +39,20 @@ houseNuts.pledge();
 // Principle 3
 
 // code example for New Binding
+function BannerMan(watcher) {
+    this.command = 'Halt';
+    this.check = watcher;
+    this.speak = function() {
+        console.log(this.command + this.check);
+        console.log(this);
+    };
+}
+
+const ally = new BannerMan(' Good Sir');
+const traitor = new BannerMan(' Scoundrel');
+
+ally.speak();
+traitor.speak();
 
 // Principle 4
 
